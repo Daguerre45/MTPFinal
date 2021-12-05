ad9d2f26177e9c5dd85f350fc39c8924db480af9
Juaki15 <72501060+Juaki15@users.noreply.github.com>
2021-12-05 20:58:30 +0100

    colores de pagina principal

--- src/dominio/Interfaz.java
+++ src/dominio/Interfaz.java
@@ -1,46 +1,44 @@
 package dominio;
 
 import javax.swing.*;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.Color;
 
 public class Interfaz extends JFrame {
-    JButton plataformaX;
-    JButton plataformaP;
-    JButton plataformaPC;
-    JFrame prueba;
-    //JPanel panelX = new JPanel();
+    //Icon imageX = new ImageIcon("/imagenes/imgX.png");
+    JButton botonX = new JButton("XBOX"/*, imageX*/) ;
+    //Icon imagePC = new ImageIcon("/imagenes/imgPC.jpg");
+    JButton botonPC = new JButton("WINDOWS"/*, imagePC*/) ;
+    //Icon imagePS = new ImageIcon("/imagenes/imgPS.png");
+    JButton botonPS = new JButton("PLAY STATION"/*,imagePS*/) ;
 
-    public Interfaz() {super("Plataforma");}
-
-    public void iniciarComponentes(){
-        iniciarBotones();
-    }
-
-    private void iniciarBotones(){
-        setLayout(new GridLayout(1, 3, 5, 5));
-        for(int i = 0; i < 3; i++){
-            if(i == 0){
-                add(new JButton("Xbox"));
-            }
-            if(i == 1){
-                add(new JButton("PlayStation"));
-            }
-
-            if(i == 2){
-                add(new JButton("PC"));
-            }
-        }
-        setTitle("Plataforma");
-        setSize(1650,1080);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
+    public Interfaz(){
+        Toolkit miPantalla = Toolkit.getDefaultToolkit();
+        Image miIcono = miPantalla.getImage("imagenes/icono.jpg");
+        setIconImage(miIcono);
+        Dimension tamanoPantalla = miPantalla.getScreenSize(); //devuelve el tamaño de la pantalla. Si hay mas de un monitor, el del principal
+        int alturaPantalla = tamanoPantalla.height;
+        int anchoPantalla = tamanoPantalla.width;
+        setBounds(anchoPantalla/4,alturaPantalla/4, anchoPantalla/2, alturaPantalla/2 );
+        setTitle("Ultimate Builder");
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        JPanel lamina = new JPanel();
+        lamina.setLayout(new GridLayout(1, 3, 5, 5));
+        lamina.add(botonPC);
+        lamina.add(botonPS);
+        lamina.add(botonX);
+
+        //botonPS.setIcon(imagePS);
+        botonPS.setBackground(new java.awt.Color(0, 0, 255));
+        //botonPC.setIcon(imagePC);
+        botonPC.setBackground(new java.awt.Color(51,133,255));
+        //botonX.setIcon(imagePC);
+        botonX.setBackground(new java.awt.Color(0,255,0));
+        lamina.setVisible(true);
+        add(lamina);
     }
-
-    
-
 }

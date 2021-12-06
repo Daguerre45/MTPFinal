31b1f3f805c68d74f47a535b1ee6a7f98212b912
Juaki15 <72501060+Juaki15@users.noreply.github.com>
2021-12-06 09:56:04 +0100

    imagenes okey

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -6,9 +6,6 @@ import dominio.*;
 public class Principal {
 
     public static void main(String[] args) {
-        Toolkit miPantalla = Toolkit.getDefaultToolkit();
-        Image miIcono = miPantalla.getImage("imagenes/icono.jpg");
         Interfaz interfaz = new Interfaz();
-        interfaz.setIconImage(miIcono);
     }
 }

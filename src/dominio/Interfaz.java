35677269caca2e4ef134c5fd9a59b3ca2e83d61b
Juaki15 <72501060+Juaki15@users.noreply.github.com>
2021-12-06 09:54:55 +0100

    imagenes okey

--- src/dominio/Interfaz.java
+++ src/dominio/Interfaz.java
@@ -2,43 +2,39 @@ package dominio;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.Color;
 
 public class Interfaz extends JFrame {
-    //Icon imageX = new ImageIcon("/imagenes/imgX.png");
-    JButton botonX = new JButton("XBOX"/*, imageX*/) ;
-    //Icon imagePC = new ImageIcon("/imagenes/imgPC.jpg");
-    JButton botonPC = new JButton("WINDOWS"/*, imagePC*/) ;
-    //Icon imagePS = new ImageIcon("/imagenes/imgPS.png");
-    JButton botonPS = new JButton("PLAY STATION"/*,imagePS*/) ;
 
     public Interfaz(){
         Toolkit miPantalla = Toolkit.getDefaultToolkit();
-        Image miIcono = miPantalla.getImage("imagenes/icono.jpg");
-        setIconImage(miIcono);
-        Dimension tamanoPantalla = miPantalla.getScreenSize(); //devuelve el tamaño de la pantalla. Si hay mas de un monitor, el del principal
+        Image miIcono = miPantalla.getImage("icono.jpg");
+        Icon imageX = new ImageIcon("imgX.png");
+        JButton botonX = new JButton("", imageX) ;
+        botonX.setToolTipText("Xbox");
+        Icon imagePC = new ImageIcon("imgPC.png");
+        JButton botonPC = new JButton("", imagePC) ;
+        botonPC.setToolTipText("Windows");
+        Icon imagePS = new ImageIcon("imgPS.png");
+        JButton botonPS = new JButton("", imagePS) ;
+        botonPS.setToolTipText("Play Station");
+        JPanel lamina = new JPanel();
+        Dimension tamanoPantalla = miPantalla.getScreenSize();
         int alturaPantalla = tamanoPantalla.height;
         int anchoPantalla = tamanoPantalla.width;
         setBounds(anchoPantalla/4,alturaPantalla/4, anchoPantalla/2, alturaPantalla/2 );
         setTitle("Ultimate Builder");
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-        JPanel lamina = new JPanel();
+        add(lamina);
+        setIconImage(miIcono);
         lamina.setLayout(new GridLayout(1, 3, 5, 5));
         lamina.add(botonPC);
         lamina.add(botonPS);
         lamina.add(botonX);
 
-        //botonPS.setIcon(imagePS);
         botonPS.setBackground(new java.awt.Color(0, 0, 255));
-        //botonPC.setIcon(imagePC);
         botonPC.setBackground(new java.awt.Color(51,133,255));
-        //botonX.setIcon(imagePC);
         botonX.setBackground(new java.awt.Color(0,255,0));
         lamina.setVisible(true);
-        add(lamina);
     }
 }

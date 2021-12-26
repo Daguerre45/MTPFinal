1b2ab2a159d645c874b611558c17f623ad0a3578
Daguerre45 <alberto.daguerre02@gmail.com>
2021-12-26 11:42:07 +0100

    Menu Jugadores

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -6,6 +6,6 @@ import java.sql.SQLException;
 
 public class Principal {
     public static void main(String[] args) throws SQLException {
-        Inicio inicio = new Inicio();
+        Jugadores jugadores = new Jugadores();
     }
 }
